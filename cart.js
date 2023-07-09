01e7c628236cc2f2696eadbdb895f1dd0d64d093
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2023-07-09 23:12:10 +0800

    added file + functionalities
    1) Added git ignore file
    2) Refactoring code + added image picture + added deleting of items in side cart

--- cart.js
+++ cart.js
@@ -5,6 +5,7 @@ const cartItemContainer = document.querySelector("[data-cart-items]")
 const shoppingCartIcon = document.querySelector("[data-cart]")
 const sideCheckoutContainer = document.querySelector("[data-item-holder]")
 const cartItemTemplate = document.querySelector('#side-item-template')
+const imageInBasketPath = "https://dummyimage.com/210x130/"
 let shoppingCart = []
 
 export function setupShoppingCart() {
@@ -19,18 +20,29 @@ export function addItemsToCart(id) {
 }
 
 function renderSideCart() {
-  const cartItem = cartItemTemplate.content.cloneNode(true)
-  const name = cartItem.querySelector("[data-name]")
-  const price = cartItem.querySelector("[data-price-side]")
-  const image = cartItem.querySelector("")
-
-
+  cartItemContainer.innerHTML = ''
   shoppingCart.forEach(entry => {
+    const cartItem = cartItemTemplate.content.cloneNode(true)
+    const name = cartItem.querySelector("[data-name]")
+    const price = cartItem.querySelector("[data-price-side]")
+    const image = cartItem.querySelector("[data-image]")
+    const removeItemFromCart = cartItem.querySelector("[data-remove-from-cart-button]")
     const item = items.find(i => entry.id === i.id)
     name.innerText = item.name
     price.innerText = currencyFormatter(item.priceCents / 100)
-
-
+    image.src = `${imageInBasketPath}` + item.imageColor
+    cartItemContainer.appendChild(cartItem)
+    removeItemFromCart.addEventListener('click', removeCartItemHandler)
   })
-  cartItemContainer.appendChild(cartItem)
+}
+
+function removeCartItemHandler(event) {
+  if (event.target.matches("[data-remove-from-cart-button]")) {
+    const itemSide = event.target.closest("[data-item-side]")
+    const index = Array.from(cartItemContainer.children).indexOf(itemSide)
+    if (index !== -1) {
+      shoppingCart.splice(index, 1)
+    }
+    itemSide.remove()
+  }
 }
